b0b72b8f73c42e567248c9d82f0fd62c7dcdf080
Den Leschenko <den.leschenko@gmail.com>
2024-10-11 08:51:44 +0300

    update packages; Change Convert method to public in SpecialFolderPatternConverter;

--- src/Greenshot.Base/Core/LogHelper.cs
+++ src/Greenshot.Base/Core/LogHelper.cs
@@ -115,7 +115,7 @@ namespace Greenshot.Base.Core
     /// </summary>
     public class SpecialFolderPatternConverter : PatternConverter
     {
-        protected override void Convert(TextWriter writer, object state)
+        public override void Convert(TextWriter writer, object state)
         {
             Environment.SpecialFolder specialFolder = (Environment.SpecialFolder) Enum.Parse(typeof(Environment.SpecialFolder), Option, true);
             writer.Write(Environment.GetFolderPath(specialFolder));
